99f08206c7f33bbeae47092b15b8b6b1ede6cfde
Maorguez <avila.mauro@gmail.com>
2020-12-10 14:26:26 -0500

    Adding useRef on component information

--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -18,8 +18,16 @@ const useinitialState = () => {
     });
   }
 
+  const addToBuyer = payload => {
+    setState({
+      ...state,
+      buyer: [...state.buyer, payload],
+    });
+  }
+
   return {
     addToCart,
+    addToBuyer,
     removeFromCart,
     state,
   };
